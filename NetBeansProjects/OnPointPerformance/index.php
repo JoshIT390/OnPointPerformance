549891f93d0a1444d5ed2b5610ccfcb27e61dbe8
Josh <Josh@10.0.0.6>
2016-02-28 15:51:28 -0500

    Added slideshow to splash page

--- NetBeansProjects/OnPointPerformance/index.php
+++ NetBeansProjects/OnPointPerformance/index.php
@@ -16,6 +16,65 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>On Point Performance Center</title>
         <?php include ("./assets/virtual/mainBootstrap.inc"); ?>
+        
+        <link rel="stylesheet" href="./assets/slideshow/css/supersized.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="./assets/slideshow/theme/supersized.shutter.css" type="text/css" media="screen" />
+		
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+        <script type="text/javascript" src="./assets/slideshow/js/jquery.easing.min.js"></script>
+		
+        <script type="text/javascript" src="./assets/slideshow/js/supersized.3.2.7.js"></script>
+        <script type="text/javascript" src="./assets/slideshow/theme/supersized.shutter.js"></script>
+		
+	<script type="text/javascript">
+            jQuery(function($){
+                $.supersized({
+                    // Functionality
+                    slideshow : 1, // Slideshow on/off
+                    autoplay : 1, // Slideshow starts playing automatically
+                    start_slide : 1, // Start slide (0 is random)
+                    stop_loop :	0, // Pauses slideshow on last slide
+                    random : 0, // Randomize slide order (Ignores start slide)
+                    slide_interval : 3000, // Length between transitions
+                    transition : 6, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                    transition_speed : 1000, // Speed of transition
+                    new_window : 1, // Image links open in new window/tab
+                    pause_hover : 0, // Pause slideshow on hover
+                    keyboard_nav : 1, // Keyboard navigation on/off
+                    performance : 1, // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+                    image_protect : 1, // Disables image dragging and right click with Javascript
+													   
+                    // Size & Position						   
+                    min_width : 0, // Min width allowed (in pixels)
+                    min_height : 0, // Min height allowed (in pixels)
+                    vertical_center : 1, // Vertically center background
+                    horizontal_center : 1, // Horizontally center background
+                    fit_always : 0, // Image will never exceed browser width or height (Ignores min. dimensions)
+                    fit_portrait : 1, // Portrait images will not exceed browser height
+                    fit_landscape : 0, // Landscape images will not exceed browser width
+															   
+                    // Components							
+                    slide_links	: 'blank', // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+                    thumb_links	: 1, // Individual thumb links for each slide
+                    thumbnail_navigation : 0, // Thumbnail navigation
+                    slides : [	 // Slideshow Images
+                        {image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-1.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-1.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-2.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-2.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-3.jpg', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-3.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-1.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-1.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-2.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-2.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-3.jpg', title : 'Image Credit: Colin Wojno', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-3.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-1.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-1.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-2.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-2.jpg'},
+			{image : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-3.jpg', title : 'Image Credit: Brooke Shaden', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-3.jpg'}
+                    ],
+
+                    // Theme Options			   
+                    progress_bar : 3, // Timer for each slide							
+                    mouse_scrub	: 0	
+		});
+	    });
+	</script>
     </head>
     <body>
         <nav class="navbar navbar-default">
@@ -58,19 +117,52 @@ and open the template in the editor.
                 </div>
             </div>
         </nav>
+        <div class="container">
         
-        <div class="jumbotron">
-            <h1>Splash Page</h1>
-            <p>to do: add slides</p>
-            <div>
-                
+	<!--Thumbnail Navigation-->
+	<div id="prevthumb"></div>
+        <div id="nextthumb"></div>
+	
+        <!--Arrow Navigation-->
+        <a id="prevslide" class="load-item"></a>
+        <a id="nextslide" class="load-item"></a>
+	
+        <div id="thumb-tray" class="load-item">
+            <div id="thumb-back"></div>
+            <div id="thumb-forward"></div>
+	</div>
+	
+	<!--Time Bar-->
+	<div id="progress-back" class="load-item">
+            <div id="progress-bar"></div>
+	</div>
+	
+	<!--Control Bar-->
+	<div id="controls-wrapper" class="load-item">
+            <div id="controls">
+                <a id="play-button"><img id="pauseplay" src="./assets/slideshow/img/pause.png"/></a>
+		
+		<!--Slide counter-->
+		<div id="slidecounter">
+                    <span class="slidenumber"></span> / <span class="totalslides"></span>
+		</div>
+			
+		<!--Slide captions displayed here-->
+		<div id="slidecaption"></div>
+			
+		<!--Thumb Tray button-->
+                <a id="tray-button"><img id="tray-arrow" src="./assets/slideshow/img/button-tray-up.png"/></a>
+			
+		<!--Navigation-->
+		<ul id="slide-list"></ul>	
             </div>
+	</div>
         </div>
-        <p><a href="./assets/images/ph_300x300.gif" download>test</a></p>
-        <div class="panel panel-default">
+        <!--
+        <div class="panel panel-default" style="margin-bottom: 0px; padding-top: 2px;">
             <div class="panel-footer">
                 <?php include ("./assets/virtual/footer.inc"); ?>
             </div>
-        </div>
+        </div>-->
     </body>
 </html>
