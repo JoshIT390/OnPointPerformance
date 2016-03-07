08120c84b2c40eeae27a5611cc833c3713b6e487
tatumkids <tatumkids@marks-air.home>
2016-03-07 12:43:53 -0500

    test

--- NetBeansProjects/OnPointPerformance/merchandise/index.php
+++ NetBeansProjects/OnPointPerformance/merchandise/index.php
@@ -62,7 +62,13 @@ and open the template in the editor.
         
         <div class="jumbotron">
   <h1>Jumbotron</h1>
-  <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <p>Merchandise Item 1</p>
+  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+</div>
+        
+           <div class="jumbotron">
+  <h1>Jumbotron</h1>
+  <p>Merchandise Item 2</p>
   <p><a class="btn btn-primary btn-lg">Learn more</a></p>
 </div>
         
