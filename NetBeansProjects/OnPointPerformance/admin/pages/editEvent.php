b86e97ae0f581ef32856cebb07435901e48a4cc5
Owner <Owner@Owner-VAIO.eduroam.gmu.edu>
2016-03-08 16:29:46 -0500

    Updated viewEvent, editEvent, and updateEvent

---
+++ NetBeansProjects/OnPointPerformance/admin/pages/editEvent.php
@@ -0,0 +1,194 @@
+<?php
+    session_start();
+    if($_SERVER['SERVER_PORT'] != '443') { 
+        header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']); 
+        exit();
+    }
+    
+    // Redirects to login page if haven't logged in or trying to access page as admin
+    if (isset($_SESSION['member_username'])){
+        unset($_SESSION['member_username']);
+    }
+    elseif (!isset($_SESSION['admin_username'])) {
+        header("Location: ../../login");
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OPPC Admin Page</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	<!-- Inline Forms -->
+    <link href="inline.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">On Point Performance Administration Page</a>
+            </div>
+            
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="../../login/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            
+                        </li>
+                        <li>
+                            <a href="./"><i class="fa fa-dashboard fa-fw"></i> Member Management</a>
+                        </li>
+                        <li>
+                            <a href="./calendar.php"><i class="fa fa-table fa-fw"></i> Manage Calendar</a>
+                        </li>
+                        <li>
+                            <a href="./email.php"><i class="fa fa-edit fa-fw"></i> Email Members</a>
+                        </li>
+						<li>
+                            <a href="./applications.php"><i class="fa fa-edit fa-fw"></i> View Applications</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Website Management<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="./bannerm.php">Front Page Banner</a>
+                                </li>
+                                <li>
+                                    <a href="./announcementsm.php">Front Page Announcements</a>
+                                </li>
+								<li>
+                                    <a href="formsm.php">Forms</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Manage Calendar Events</h1>
+						<p> <?php
+							$servername = "mysql.dnguyen94.com";
+							$username = "ad_victorium";
+							$password = "MT8AlJAM";
+							$database = "onpoint_performance_center_lower";
+
+							// Create connection
+							$conn = mysqli_connect($servername, $username, $password, $database);
+							$calendarID=$_POST['calendarID'];
+							// Check connection
+							if ($conn->connect_error) {
+								die("Connection failed: " . $conn->connect_error);
+							}
+							$result = mysqli_query($conn, "SELECT NAME, DATE, CITY, STATE, ZIP, DESCRIPTION, FORMS FROM CALENDAR WHERE CALENDAR_ID='" . $calendarID . "';");
+							if ($result->num_rows > 0) {
+								// output data of each row
+								while($row = $result->fetch_assoc()) {
+								echo'<form action="updateEvent.php" method="post">
+								<p>Event Name: <input type="text" name="name" value="'.$row["NAME"].'" />
+								Date: <input type="text" name="date" value="'.$row["DATE"].'"/></p>
+								<p>City: <input type="text" name="city" value="'.$row["CITY"].'"/>
+								State: <input type="text" name="state" value="'.$row["STATE"].'"/></p>
+								<p>Zip: <input type="text" name="zip" value="'.$row["ZIP"].'"/>
+								Description: <input type="text" name="description" value="'.$row["DESCRIPTION"].'"/></p>
+								<p>Forms: <input type="text" name="forms" value="'.$row["FORMS"].'"/></p>
+								<p><input type="hidden" name="calendarID" value="'.$calendarID.'"/></p>
+								<p><input type="submit" value="Update"/></p>
+								</form>';
+								
+								}
+							}
+							$result->close();
+							
+							?>  </p>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
