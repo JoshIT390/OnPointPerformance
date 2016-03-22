918150fd72996e2e2b49500a944a4f4e71e186cf
Dan <Dan@DT-DanNguyen>
2016-03-22 18:44:06 -0400

    Added framework to homepage to support new footer

--- NetBeansProjects/OnPointPerformance/index.php
+++ NetBeansProjects/OnPointPerformance/index.php
@@ -31,121 +31,123 @@ and open the template in the editor.
         <!-- END FLEXSLIDER IMPORTS -->
     </head>
     <body>
-        <!-- NAVBAR -->
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="./"> <img src="./assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
-                </div>
+        <div class="wrap">
+            <!-- NAVBAR -->
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="./"> <img src="./assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
+                    </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a><span class="sr-only">(current)</span>Home</a></li>
-                
-                        <li><a href="./about/">About Us</a></li>
-                        <li><a href="./apply/">Apply</a></li>
-                        <li><a href="./events/">Events</a></li>
-                        <li><a href="./merchandise/">Merchandise</a></li>
-                        <li><a href="./contact/">Contact Us</a></li>
-                    </ul>    
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php
-                            if (isset($_SESSION['member_username'])){
-                                echo '<li><a href="./members">' . $_SESSION['member_username'] . '</a></li>';
-                                echo '<li><a href="./login/logout.php">Logout</a></li>';
-                            }
-                            if (isset($_SESSION['admin_username'])){
-                                echo '<li><a href="./admin">' . $_SESSION['admin_username'] . '</a></li>';
-                                echo '<li><a href="./login/logout.php">Logout</a></li>';                            
-                            }
-                            elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
-                                echo '<li><a href="./login">Log In</a></li>';
-                            }
-                        ?>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a><span class="sr-only">(current)</span>Home</a></li>
+
+                            <li><a href="./about/">About Us</a></li>
+                            <li><a href="./apply/">Apply</a></li>
+                            <li><a href="./events/">Events</a></li>
+                            <li><a href="./merchandise/">Merchandise</a></li>
+                            <li><a href="./contact/">Contact Us</a></li>
+                        </ul>    
+                        <ul class="nav navbar-nav navbar-right">
+                            <?php
+                                if (isset($_SESSION['member_username'])){
+                                    echo '<li><a href="./members">' . $_SESSION['member_username'] . '</a></li>';
+                                    echo '<li><a href="./login/logout.php">Logout</a></li>';
+                                }
+                                if (isset($_SESSION['admin_username'])){
+                                    echo '<li><a href="./admin">' . $_SESSION['admin_username'] . '</a></li>';
+                                    echo '<li><a href="./login/logout.php">Logout</a></li>';                            
+                                }
+                                elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
+                                    echo '<li><a href="./login">Log In</a></li>';
+                                }
+                            ?>
+                        </ul>
+                    </div>
+     <a> <img src="./assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>               
+                </div>
+            </nav>
+            <!-- END NAVBAR -->
+
+            <!-- FLEXSLIDER -->
+            <div class="container">
+                <div class="flexslider" style="width:100%; height:auto; position:relative; display:block; margin-left:auto; margin-right:auto; background:#000000; border: 5px solid #000000;">
+                    <ul class="slides" style="width:auto; height:auto; display:block; margin-left:auto; margin-right:auto;">
+                        <li>
+                            <img src="./assets//images/slide1.jpg" style="height:auto;" />
+                            <div class="flex-caption">
+                                <h1>Dual Purpose Gym</h1>
+                                <h4>We have our facility split into two sides, one for strength training and the other tactical training</h4>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="./assets/images/slide2.jpg" style="height:auto;"/>
+                            <div class="flex-caption">
+                                <h1>Strength Training</h1>
+                                <h4>Our strength training section has all the equipment you needed to get stronger</h4>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="./assets/images/slide3.jpg" style="height:auto;"/>
+                            <div class="flex-caption">
+                                <h1>Tactical Training</h1>
+                                <h4>Our tactical section is set up for practicing military and police routines</h4>
+                            </div>
+                        </li>
                     </ul>
                 </div>
- <a> <img src="./assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>               
             </div>
-        </nav>
-        <!-- END NAVBAR -->
-        
-        <!-- FLEXSLIDER -->
-        <div class="container">
-            <div class="flexslider" style="width:100%; height:auto; position:relative; display:block; margin-left:auto; margin-right:auto; background:#000000; border: 5px solid #000000;">
-                <ul class="slides" style="width:auto; height:auto; display:block; margin-left:auto; margin-right:auto;">
-                    <li>
-                        <img src="./assets//images/slide1.jpg" style="height:auto;" />
-                        <div class="flex-caption">
-                            <h1>Dual Purpose Gym</h1>
-                            <h4>We have our facility split into two sides, one for strength training and the other tactical training</h4>
+            <!-- END FLEXSLIDER -->
+
+            <!-- ANNOUNCEMENTS 
+            <div class="container" style="width: auto;">
+                <div class="row-fluid" style="margin-bottom: 2em;">
+                    <div class="col-lg-3" style="text-align: center;">
+                        <img src="./assets/images/ph_300x300.gif" alt="placeholder">
+                    </div>
+                    <div class="col-lg-9"style="text-align: center;">
+                        <div class="jumbotron" style="height: 300px;">
+                            <h1>Announcement 1</h1>
+                            <p>Announcement text will go here.</p>
                         </div>
-                    </li>
-                    <li>
-                        <img src="./assets/images/slide2.jpg" style="height:auto;"/>
-                        <div class="flex-caption">
-                            <h1>Strength Training</h1>
-                            <h4>Our strength training section has all the equipment you needed to get stronger</h4>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="./assets/images/slide3.jpg" style="height:auto;"/>
-                        <div class="flex-caption">
-                            <h1>Tactical Training</h1>
-                            <h4>Our tactical section is set up for practicing military and police routines</h4>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <!-- END FLEXSLIDER -->
-        
-        <!-- ANNOUNCEMENTS 
-        <div class="container" style="width: auto;">
-            <div class="row-fluid" style="margin-bottom: 2em;">
-                <div class="col-lg-3" style="text-align: center;">
-                    <img src="./assets/images/ph_300x300.gif" alt="placeholder">
+                    </div>
                 </div>
-                <div class="col-lg-9"style="text-align: center;">
-                    <div class="jumbotron" style="height: 300px;">
-                        <h1>Announcement 1</h1>
-                        <p>Announcement text will go here.</p>
+
+                <div class="row-fluid" style="margin-bottom: 2em;">
+                    <div class="col-lg-3"style="text-align: center;">
+                        <img src="./assets/images/ph_300x300.gif" alt="placeholder">
+                    </div>
+                    <div class="col-lg-9"style="text-align: center;">
+                        <div class="jumbotron" style="height: 300px;">
+                            <h1>Announcement 2</h1>
+                            <p>Announcement text will go here.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row-fluid">
+                    <div class="col-lg-3"style="text-align: center;">
+                        <img src="./assets/images/ph_300x300.gif" alt="placeholder">
+                    </div>
+                    <div class="col-lg-9"style="text-align: center;">
+                        <div class="jumbotron" style="height: 300px;">
+                            <h1>Announcement 3</h1>
+                            <p>Announcement text will go here.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-            <div class="row-fluid" style="margin-bottom: 2em;">
-                <div class="col-lg-3"style="text-align: center;">
-                    <img src="./assets/images/ph_300x300.gif" alt="placeholder">
-                </div>
-                <div class="col-lg-9"style="text-align: center;">
-                    <div class="jumbotron" style="height: 300px;">
-                        <h1>Announcement 2</h1>
-                        <p>Announcement text will go here.</p>
-                    </div>
-                </div>
+                    -->
+                     <div class="container-fluid">
+                <?php include 'announcements.php';?>
             </div>
-                
-            <div class="row-fluid">
-                <div class="col-lg-3"style="text-align: center;">
-                    <img src="./assets/images/ph_300x300.gif" alt="placeholder">
-                </div>
-                <div class="col-lg-9"style="text-align: center;">
-                    <div class="jumbotron" style="height: 300px;">
-                        <h1>Announcement 3</h1>
-                        <p>Announcement text will go here.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-		-->
-		 <div class="container-fluid">
-            <?php include 'announcements.php';?>
         </div>
         <!-- END ANNOUNCEMENTS -->
         
@@ -158,16 +160,7 @@ and open the template in the editor.
         </div>-->
         
         <!-- FOOTER -->
-        <div class="well">
-            <?php include ("./assets/virtual/footer.inc"); ?>
-        </div>
-        <!-- END FOOTER -->
-        
-        <!--
-        <div class="panel panel-default" style="margin-bottom: 0px; padding-top: 2px;">
-            <div class="panel-footer">
-                
-            </div>
-        </div>-->
+        <?php include ("./assets/virtual/footer_home.inc"); ?>
+
     </body>
 </html>
