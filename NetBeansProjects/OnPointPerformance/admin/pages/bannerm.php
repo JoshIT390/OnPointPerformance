5fbcb08e3b2b5bc2fdf9e45e50ef83f638d0d1b3
Monstar <Monstar@10.0.1.43>
2016-03-21 22:38:55 -0400

    Fixed an issue in the admin portal

--- NetBeansProjects/OnPointPerformance/admin/pages/bannerm.php
+++ NetBeansProjects/OnPointPerformance/admin/pages/bannerm.php
@@ -64,23 +64,6 @@
                 <a class="navbar-brand" href="index.php">On Point Performance Administration Page</a>
             </div>
             
-            <ul class="nav navbar-top-links navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="../../login/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            
              <!-- /.navbar-header -->
              
             <ul class="nav navbar-top-links navbar-right">
