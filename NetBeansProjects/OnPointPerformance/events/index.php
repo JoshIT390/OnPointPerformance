ca0526d0ba8bd0e5f332229e7d383d366382c841
Dan <Dan@DT-DanNguyen>
2016-03-22 21:16:15 -0400

    Added framework to about, events, and merchandise page to support new footer. Put page contents of merchandise into container div and row-fluid divs to support whatever is done to that page later on.

--- NetBeansProjects/OnPointPerformance/events/index.php
+++ NetBeansProjects/OnPointPerformance/events/index.php
@@ -29,77 +29,74 @@ and open the template in the editor.
         <!-- END FLEXSLIDER IMPORTS -->
     </head>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                   <a href="../"> <img src="../assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
-                </div>
+        <div class="wrap">
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                       <a href="../"> <img src="../assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
+                    </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="../">Home</a></li>
-                        <li><a href="../about/">About Us</a></li>
-                        <li><a href="../apply/">Apply</a></li>
-                        <li class="active"><a href=><span class="sr-only">(current)</span>Events</a></li>
-                        <li><a href="../merchandise/">Merchandise</a></li>
-                        <li><a href="../contact/">Contact Us</a></li>
-                    </ul>    
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php
-                            if (isset($_SESSION['member_username'])){
-                                echo '<li><a href="../members">' . $_SESSION['member_username'] . '</a></li>';
-                                echo '<li><a href="../login/logout.php">Logout</a></li>';
-                            }
-                            if (isset($_SESSION['admin_username'])){
-                                echo '<li><a href="../admin">' . $_SESSION['admin_username'] . '</a></li>';
-                                echo '<li><a href="../login/logout.php">Logout</a></li>';                            
-                            }
-                            elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
-                                echo '<li><a href="../login">Log In</a></li>';
-                            }
-                        ?>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="../">Home</a></li>
+                            <li><a href="../about/">About Us</a></li>
+                            <li><a href="../apply/">Apply</a></li>
+                            <li class="active"><a href=><span class="sr-only">(current)</span>Events</a></li>
+                            <li><a href="../merchandise/">Merchandise</a></li>
+                            <li><a href="../contact/">Contact Us</a></li>
+                        </ul>    
+                        <ul class="nav navbar-nav navbar-right">
+                            <?php
+                                if (isset($_SESSION['member_username'])){
+                                    echo '<li><a href="../members">' . $_SESSION['member_username'] . '</a></li>';
+                                    echo '<li><a href="../login/logout.php">Logout</a></li>';
+                                }
+                                if (isset($_SESSION['admin_username'])){
+                                    echo '<li><a href="../admin">' . $_SESSION['admin_username'] . '</a></li>';
+                                    echo '<li><a href="../login/logout.php">Logout</a></li>';                            
+                                }
+                                elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
+                                    echo '<li><a href="../login">Log In</a></li>';
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                    <a> <img src="../assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>
+                </div>
+            </nav>
+
+            <!-- FLEXSLIDER -->
+            <div class="container">
+                <div class="flexslider" style="width:100%; height:auto; position:relative; display:block; margin-left:auto; margin-right:auto; background:#000000; border: 5px solid #000000;">
+                    <ul class="slides" style="width:auto; height:auto; display:block; margin-left:auto; margin-right:auto;">
+                        <li>
+                            <img src="../assets/images/eventsImage.jpg" style="height:auto;" />
+                            <div class="flex-caption">
+                                <h1>Upcoming Events</h1>
+                                <h4>Come Join Us for these special events.</h4>
+                            </div>
+                        </li>
                     </ul>
                 </div>
-                <a> <img src="../assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>
             </div>
-        </nav>
-        
-        <!-- FLEXSLIDER -->
-        <div class="container">
-            <div class="flexslider" style="width:100%; height:auto; position:relative; display:block; margin-left:auto; margin-right:auto; background:#000000; border: 5px solid #000000;">
-                <ul class="slides" style="width:auto; height:auto; display:block; margin-left:auto; margin-right:auto;">
-                    <li>
-                        <img src="../assets/images/eventsImage.jpg" style="height:auto;" />
-                        <div class="flex-caption">
-                            <h1>Upcoming Events</h1>
-                            <h4>Come Join Us for these special events.</h4>
-                        </div>
-                    </li>
-                </ul>
+            <!-- END FLEXSLIDER -->
+
+
+            <div class="container">
+            <!--<div class="jumbotron" style="text-align: center;">
+                    <h1>Upcoming Events</h1>
+                    <p>Come join us.</p>
+                </div>
+            -->
+                <?php include 'events.php'; ?>
             </div>
         </div>
-        <!-- END FLEXSLIDER -->
-        
-        
-        <div class="container">
-        <!--<div class="jumbotron" style="text-align: center;">
-                <h1>Upcoming Events</h1>
-                <p>Come join us.</p>
-            </div>
-        -->
-            <?php include 'events.php'; ?>
-        </div>
-        
-        <div class="panel panel-default">
-            <div class="panel-footer">
-                <?php include ("../assets/virtual/footer.inc"); ?>
-            </div>
-        </div>
+        <?php include ("../assets/virtual/footer.inc"); ?>
     </body>
 </html>
--- NetBeansProjects/OnPointPerformance/merchandise/index.php
+++ NetBeansProjects/OnPointPerformance/merchandise/index.php
@@ -18,78 +18,75 @@ and open the template in the editor.
         <?php include ("../assets/virtual/mainBootstrap2.inc"); ?>
     </head>
     <body>
-        <nav class="navbar navbar-default">
+        <div class="wrap">
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="../"> <img src="../assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="../">Home</a></li>
+                            <li><a href="../about/">About Us</a></li>
+                            <li><a href="../apply/">Apply</a></li>
+                            <li><a href="../events/">Events</a></li>
+                            <li class="active"><a><span class="sr-only">(current)</span>Merchandise</a></li>
+                            <li><a href="../contact/">Contact Us</a></li>
+                        </ul>    
+                        <ul class="nav navbar-nav navbar-right">
+                            <?php
+                                if (isset($_SESSION['member_username'])){
+                                    echo '<li><a href="../members">' . $_SESSION['member_username'] . '</a></li>';
+                                    echo '<li><a href="../login/logout.php">Logout</a></li>';
+                                }
+                                if (isset($_SESSION['admin_username'])){
+                                    echo '<li><a href="../admin">' . $_SESSION['admin_username'] . '</a></li>';
+                                    echo '<li><a href="../login/logout.php">Logout</a></li>';                            
+                                }
+                                elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
+                                    echo '<li><a href="../login">Log In</a></li>';
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                    <a> <img src="../assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>
+                </div>
+            </nav>
+
             <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="../"> <img src="../assets/images/Logo.png" style="width:220px; height:50px;float: left;"> </a>
+                <div class="row-fluid">
+                    <div class="jumbotron">
+                        <h1>Jumbotron</h1>
+                        <p>Merchandise Item 1</p>
+                    </div>
                 </div>
-
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li><a href="../">Home</a></li>
-                        <li><a href="../about/">About Us</a></li>
-                        <li><a href="../apply/">Apply</a></li>
-                        <li><a href="../events/">Events</a></li>
-                        <li class="active"><a><span class="sr-only">(current)</span>Merchandise</a></li>
-                        <li><a href="../contact/">Contact Us</a></li>
-                    </ul>    
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php
-                            if (isset($_SESSION['member_username'])){
-                                echo '<li><a href="../members">' . $_SESSION['member_username'] . '</a></li>';
-                                echo '<li><a href="../login/logout.php">Logout</a></li>';
-                            }
-                            if (isset($_SESSION['admin_username'])){
-                                echo '<li><a href="../admin">' . $_SESSION['admin_username'] . '</a></li>';
-                                echo '<li><a href="../login/logout.php">Logout</a></li>';                            
-                            }
-                            elseif (!isset($_SESSION['member_username']) && !isset($_SESSION['admin_username'])) {
-                                echo '<li><a href="../login">Log In</a></li>';
-                            }
-                        ?>
-                    </ul>
+                <div class="row-fluid">
+                   <div class="jumbotron">
+                        <h1>Jumbotron</h1>
+                        <p>Merchandise Item 2</p>
+                        <p>
+                            <a class="btn btn-primary btn-lg">Learn more</a>
+                        </p>
+                    </div>
                 </div>
-                <a> <img src="../assets/images/red slash.png" style="width:100%; height:15px;float: left;"> </a>
-            </div>
-        </nav>
-       
-        <div class="container-fluid">
-            
-        <div class="col-lg-9">
-        <div class="jumbotron">
-  <h1>Jumbotron</h1>
-
-  <p>Merchandise Item 1</p>
-  
-</div>
-        </div>
-        <div class="col-lg-9">
-           <div class="jumbotron">
-  <h1>Jumbotron</h1>
-  <p>Merchandise Item 2</p>
-  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
-</div>
-        </div>
-         <div class="col-lg-9">
-           <div class="jumbotron">
-  <h1>Jumbotron</h1>
-  <p>Merchandise Item 3</p>
-  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
-</div>
-        </div>
-        </div>
-        
-        <div class="panel panel-default">
-            <div class="panel-footer">
-                <?php include ("../assets/virtual/footer.inc"); ?>
-                
+                <div class="row-fluid">
+                    <div class="jumbotron">
+                        <h1>Jumbotron</h1>
+                        <p>Merchandise Item 3</p>
+                        <p>
+                            <a class="btn btn-primary btn-lg">Learn more</a>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
+        <?php include ("../assets/virtual/footer.inc"); ?>
     </body>
 </html>
