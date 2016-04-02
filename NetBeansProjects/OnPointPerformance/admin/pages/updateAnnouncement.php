202ba604589b63bfe5169b207b61e0fed25d6e89
Monstar <Monstar@10.0.1.43>
2016-04-02 17:50:21 -0400

    found it!

--- NetBeansProjects/OnPointPerformance/admin/pages/updateAnnouncement.php
+++ NetBeansProjects/OnPointPerformance/admin/pages/updateAnnouncement.php
@@ -73,7 +73,9 @@
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        <li><a href="./profile/"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="../../"><i class="fa fa-home fa-fw"></i> Public Website</a>
                         </li>
                         <li class="divider"></li>
                         <li><a href="../../login/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
